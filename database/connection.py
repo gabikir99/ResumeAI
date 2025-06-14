ca15d2557ee9f9caa2aaab8c89d069f2f9e07b62
Adel Akhmed <59234594+adelxh@users.noreply.github.com>
2025-06-14 16:32:30 -0400

    dotenv loaded in the database

--- database/connection.py
+++ database/connection.py
@@ -3,7 +3,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 from contextlib import contextmanager
+
+load_dotenv()
 
 # Database configuration
 DATABASE_URL = os.getenv('DATABASE_URL')
